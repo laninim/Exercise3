d202832df16250d5f07871c24ca19c8e5c4c33af
Mauro Lanini <mallettomachico@gmail.com>
2022-11-03 11:20:22 +0100

    Final commit

---
+++ src/it/develhope/Exercise3/House.java
@@ -0,0 +1,13 @@
+package it.develhope.Exercise3;
+
+public class House {
+
+    public String address;
+    public int numberOfFloors;
+
+    public House (String address, int numberOfFloors) {
+        this.address = address;
+        this.numberOfFloors = numberOfFloors;
+        System.out.println("Object House, created..");
+    }
+}
---
+++ src/it/develhope/Exercise3/Tester.java
@@ -0,0 +1,9 @@
+package it.develhope.Exercise3;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        House house1 = new House("35 purple street",1);
+        House house2 = new House("23 Orange street",4);
+    }
+}
